62e017cd0a2504e80041396f259e01d9b58d6a61
kiyonya <481366085@qq.com>
2025-11-03 23:45:56 +0800

    嗨嗨嗨

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,20 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
     九九乘法表();
     pascal_triangle(8);
     calc_sum(100);
-    print!("rust太他妈的快了")
+    print!("rust太他妈的快了");
+    array();
+    hello_world();
+    fio();
 }
-
 
 fn 九九乘法表(){
     for i in 1..=9 {
         for j in 1..=i {
-            print!("{}×{}={:<2}  ", j, i, i * j);
+            print!("{}X{}={:<2}  ", j, i, i * j);
         }
         println!();
     }
@@ -40,4 +45,39 @@ fn calc_sum(n:i32){
         sum += i
     }
     print!("从0到{}的和为{}",n,sum)
+}
+
+
+fn hello_world(){
+    print!("hello world")
+}
+
+fn array(){
+    let mut array:[&str;3] = [
+        "tomorin",
+        "soyorin",
+        "rikki"
+    ];
+    print!("第一个是{}\n",array[0]);
+    print!("最后一个是{}\n",array[array.len() - 1]);
+
+    let mut any_string_array:Vec<String> = Vec::new();
+    for i in 0..=10 {
+        let bl = String::from("anon");
+        let ba = i.to_string();
+        any_string_array.push(bl + &ba);
+    }
+    let joined = any_string_array.join("\t");
+
+    print!("{}",joined);
+}
+
+
+
+fn fio() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("D:/Project/mygit/tomorin.cpp")?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    println!("文件内容:\n{}", content);
+    Ok(())
 }
